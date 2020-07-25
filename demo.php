16c19dd1433c2b2640849c38b8aac2686c6c6923
917549390 <917549390@qq.com>
2020-07-25 20:09:02 +0800

    第一次提交

---
+++ demo.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/7/25 0025
+ * Time: 19:32
+ */
+require_once './vendor/autoload.php';
+use Login\Login;
+Login::Hello();
---
+++ src/Login/Login.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/7/25 0025
+ * Time: 19:11
+ */
+namespace Login;
+
+class Login{
+    public function __construct()
+    {
+
+    }
+
+    public function Login(){
+        return  '这是登录版本1.0.0';
+    }
+
+    public static function Hello(){
+        return "Hello world";
+    }
+
+
+}
